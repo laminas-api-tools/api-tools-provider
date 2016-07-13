263a31f379cfb8a774ebc618059425ab1b055bc7
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-13 12:55:21 -0500

    Marked `Module` as deprecated.

--- src/Module.php
+++ src/Module.php
@@ -8,6 +8,9 @@ namespace ZF\Apigility\Provider;
 
 /**
  * Provided for backwards compatibility only; not required in new applications.
+ *
+ * @deprecated since 1.2.0; you no longer need to register this package as a
+ *     module in your applications.
  */
 class Module
 {
