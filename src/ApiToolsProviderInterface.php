6b441782c146b933156e8b3bf1fde649ceb1cd44
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-06-02 15:06:35 -0500

    qa: update to laminas-coding-standard 2.3 series
    Fixes issues observed in CI around deprecation warnings, which lead to CS failures.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/ApiToolsProviderInterface.php
+++ src/ApiToolsProviderInterface.php
@@ -1,10 +1,6 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-provider for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-provider/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-provider/blob/master/LICENSE.md New BSD License
- */
+declare(strict_types=1);
 
 namespace Laminas\ApiTools\Provider;
 
